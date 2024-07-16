2cb1102d0c441d62482bda2246e71cfb3b5bbf2c
Harry <harrykroby@aol.com>
2024-07-16 22:42:37 +0100

    adding backpressure to neurons

--- src/neuron.sv
+++ src/neuron.sv
@@ -7,28 +7,42 @@ module neuron (
     clk,
     rst,
 
-    a,
-    w,
-    bias,
+    neuron_ready_in,
+    neuron_valid_in,
+    neuron_data_in,
 
-    o
+    neuron_weights,
+    neuron_bias,
+
+    neuron_ready_out,
+    neuron_valid_out,
+    neuron_data_out
 );
     import cnn1d_pkg::*;
 
     parameter NUM_INPUTS = 1;
 
+    localparam NEURON_PIPE_WIDTH = LPM_PIPE_WIDTH + NUM_INPUTS;
+
     input logic clk;
     input logic rst;
 
-    input logic [DATA_WIDTH-1:0]    a       [0:NUM_INPUTS-1];
-    input logic [DATA_WIDTH-1:0]    w       [0:NUM_INPUTS-1];
-    input logic [DATA_WIDTH-1:0]    bias;
+    output logic                    neuron_ready_in;
+    input logic                     neuron_valid_in;
 
-    output logic [DATA_WIDTH-1:0]  o;
+    input logic [DATA_WIDTH-1:0]    neuron_data_in      [0:NUM_INPUTS-1];
+    input logic [DATA_WIDTH-1:0]    neuron_weights      [0:NUM_INPUTS-1];
+    input logic [DATA_WIDTH-1:0]    neuron_bias;
+
+    input logic                     neuron_ready_out;
+    output logic                    neuron_valid_out;
+    output logic [DATA_WIDTH-1:0]   neuron_data_out;
 
     logic [LPM_OUT_WIDTH-1:0] mult_out [0:NUM_INPUTS-1];
     logic [DATA_WIDTH-1:0] mult_sum [0:NUM_INPUTS-1];
     logic [DATA_WIDTH-1:0] z;
+
+    logic [NEURON_PIPE_WIDTH-1:0]  neuron_valid_in_pipe;
 
     generate
         genvar i;
@@ -37,47 +51,56 @@ module neuron (
                 .DATA_WIDTH (DATA_WIDTH),
                 .PIPE_WIDTH (LPM_PIPE_WIDTH)
             ) multiplier (
-                .clken  (1'b1),
+                .clken  (neuron_ready_in),
                 .clock  (clk),
-                .dataa  (a[i]),
-                .datab  (w[i]),
+                .dataa  (neuron_data_in[i]),
+                .datab  (neuron_weights[i]),
                 .result (mult_out[i])
             );
         end
     endgenerate
+
+    // neuron_valid_in pipeline
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            neuron_valid_in_pipe <= {NEURON_PIPE_WIDTH{1'b0}};
+        end else begin
+            if (neuron_ready_in) begin
+                // shift the input valid along the pipe only when ready is high
+                neuron_valid_in_pipe <= {neuron_valid_in_pipe[NEURON_PIPE_WIDTH-2:0],neuron_valid_in}; 
+            end
+        end
+    end
+
+    assign neuron_valid_out = neuron_valid_in_pipe[NEURON_PIPE_WIDTH-1];
 
     generate
         if (NUM_INPUTS > 1) begin
             genvar j;
             for (j=1; j<NUM_INPUTS; j++) begin: SUM_REDUCE
                 always_ff @(posedge clk) begin
-                    if (j==1) begin 
-                        mult_sum[j] <= mult_out[j-1][LPM_OUT_MSB-:DATA_WIDTH] + mult_out[j][LPM_OUT_MSB-:DATA_WIDTH];
-                    end else begin
-                        mult_sum[j] <= mult_sum[j-1] + mult_out[j][LPM_OUT_MSB-:DATA_WIDTH];
-                    end    
+                    if (neuron_ready_in) begin
+                        if (j==1) begin 
+                            mult_sum[j] <= mult_out[j-1][LPM_OUT_MSB-:DATA_WIDTH] + mult_out[j][LPM_OUT_MSB-:DATA_WIDTH];
+                        end else begin
+                            mult_sum[j] <= mult_sum[j-1] + mult_out[j][LPM_OUT_MSB-:DATA_WIDTH];
+                        end 
+                    end
                 end
             end
         end else begin
-            assign mult_sum = mult_out[0][LPM_OUT_MSB-:DATA_WIDTH];
+            assign mult_sum[NUM_INPUTS-1] = mult_out[0][LPM_OUT_MSB-:DATA_WIDTH];
         end
     endgenerate
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            z <= {DATA_WIDTH{1'b0}};
+            neuron_data_out <= {DATA_WIDTH{1'b0}};
         end else begin
-            z <= mult_sum + bias;
+            neuron_data_out <= mult_sum[NUM_INPUTS-1] + neuron_bias;
         end
     end
 
-    relu activation (
-        .clk    (clk),
-        .rst    (rst),
-        
-        .a      (z),
-        .o      (o)
-    );
-
+    assign neuron_ready_in = rst ? 1'b0 : ~neuron_valid_out | neuron_ready_out;
 
 endmodule
--- test/neuron_tb.sv
+++ test/neuron_tb.sv
@@ -6,39 +6,61 @@ module neuron_tb();
     import cnn1d_pkg::*;
     
     localparam CLK_PERIOD = 10;
-    
+    localparam NUM_INPUTS = 1;
+
     logic clk;
     logic rst;
-    logic [DATA_WIDTH-1:0] a [1];
-    logic [DATA_WIDTH-1:0] w [1];
-    logic [DATA_WIDTH-1:0] bias;
-    logic [DATA_WIDTH-1:0] o;
+
+    logic neuron_ready_in;
+    logic neuron_valid_in;
+    logic [DATA_WIDTH-1:0] neuron_data_in [0:NUM_INPUTS-1];
+    logic [DATA_WIDTH-1:0] neuron_weights [0:NUM_INPUTS-1];
+    logic [DATA_WIDTH-1:0] neuron_bias;
+    
+    logic neuron_ready_out;
+    logic neuron_valid_out;
+    logic [DATA_WIDTH-1:0] neuron_data_out;
 
     initial clk = 1'b0;
     always #(CLK_PERIOD/2) clk = ~clk;
 
-    neuron neuron(
+    neuron #(
+        .NUM_INPUTS (NUM_INPUTS)
+    ) neuron (
         .clk    (clk),
         .rst    (rst),
 
-        .a  (a),
-        .w (w),
-        .bias   (bias),
+        .neuron_ready_in    (neuron_ready_in),
+        .neuron_valid_in    (neuron_valid_in),
+        .neuron_data_in     (neuron_data_in),
 
-        .o (o)
+        .neuron_weights     (neuron_weights),
+        .neuron_bias        (neuron_bias),
+
+        .neuron_ready_out   (neuron_ready_out),
+        .neuron_valid_out   (neuron_valid_out),
+        .neuron_data_out    (neuron_data_out)
     );
 
     initial begin
-        a[0] = 0;
-        w[0] = 0;
-        bias = 0;
+        neuron_ready_out = 1'b0;
+        neuron_valid_in = 1'b0;
+        for (int i=0; i<NUM_INPUTS; i++) begin
+            neuron_data_in[i] = 0;
+            neuron_weights[i] = 0;
+        end
+        neuron_bias = 0;
         rst = 1'b1;
         repeat (3) @(posedge clk);
         rst = 1'b0;
         #20;
-        a[0] = -5;
-        w[0] = 5;
-        bias = 5;
+        neuron_ready_out = 1'b1;
+        neuron_valid_in = 1'b1;
+        for (int i=0; i<NUM_INPUTS; i++) begin
+            neuron_data_in[i] = (i+1)*5;
+            neuron_weights[i] = (i+1)*-5;
+        end
+        neuron_bias = 5;
         #4;
         #100;
     end
